9343ea780e05bf38389779d18de90268ca2bd7f1
Gregory Thompson <greg@mashwork.com>
2013-08-05 17:22:46 -0400

    Separated Query terms from each other

--- public/js/controllers/query/index.js
+++ public/js/controllers/query/index.js
@@ -3,7 +3,7 @@ function QueryIndexController($scope, $http, Query){
 	console.log("in the QueryIndexController");
 	Query.query({}, function(twitterQueryData){
 		console.log("twitterQueryData = %j", twitterQueryData);
-		$scope.query = twitterQueryData.query;
-	})
+		$scope.queryTerms = twitterQueryData.query.split(",");
+	});
 
 }
