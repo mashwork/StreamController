81592ff32bd302d72b7d1a1916b15a243273e9b1
Mattan Ingram <mattaningram@gmail.com>
2013-08-29 13:46:18 -0400

    Improved styling and layout

--- config/routes.js
+++ config/routes.js
@@ -10,24 +10,24 @@ module.exports = function (app, passport, auth) {
   app.get('/signin', users.signin)
   app.get('/signout', users.signout)
   app.post('/users/session', passport.authenticate('local', {failureRedirect: '/signin', failureFlash: 'Invalid email or password.'}), users.session)
-  app.get('/users/me', auth.requiresLogin, users.me)
-  app.get('/users/:userId', auth.requiresLogin, users.show)
+  app.get('/users/me', users.me)
+  app.get('/users/:userId', users.show)
 
   app.param('userId', users.user)
 
 
   var query = require('../app/controllers/queries');
-  app.get('/api/queries',             auth.requiresLogin, query.all);
-  app.post('/api/queries',            auth.requiresLogin, query.create);
-  app.get('/api/queries/:queryId',    auth.requiresLogin, query.one);
-  app.put('/api/queries/:queryId',    auth.requiresLogin, query.update);
-  app.del('/api/queries/:queryId',    auth.requiresLogin, query.destroy);
+  app.get('/api/queries',             query.all);
+  app.post('/api/queries',            query.create);
+  app.get('/api/queries/:queryId',    query.one);
+  app.put('/api/queries/:queryId',    query.update);
+  app.del('/api/queries/:queryId',    query.destroy);
 
   app.param('queryId', query.query)
 
   var stream = require('../app/controllers/stream');
-  app.post('/api/stream', auth.requiresLogin, stream.restart);
-  app.get('/api/stream', auth.requiresLogin, stream.show);
+  app.post('/api/stream', stream.restart);
+  app.get('/api/stream', stream.show);
 
   var partials = require('../app/controllers/partials');
   app.get('/partials/:resource/:page', partials.partials);
